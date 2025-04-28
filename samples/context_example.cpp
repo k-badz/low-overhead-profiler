b185f2f05dcd5f936a033b045ea359c686900dc1
k-badz <101342783+k-badz@users.noreply.github.com>
2025-04-28 23:50:25 +0200

    Update context_example.cpp
    forgot to add it before...

--- samples/context_example.cpp
+++ samples/context_example.cpp
@@ -5,8 +5,9 @@
 #include <chrono>
 #include <map>
 #include <string>
+#include <stdexcept>
 
-uint64_t pack_context_to_meta(std::string context_name)
+uint64_t pack_context_to_meta(std::string context_name, uint32_t flow_id = 0)
 {
     const static std::map<std::string, uint16_t> context_to_id_map = {
         {"client", 0},
@@ -20,7 +21,7 @@ uint64_t pack_context_to_meta(std::string context_name)
         throw std::runtime_error("Context name not found");
     }
 
-    return ((uint64_t)77 << 32) | (uint64_t)context_id->second;
+    return ((uint64_t)77 << 48) | ((uint64_t)context_id->second << 32) | (uint64_t)flow_id;
 }
 
 int main()
@@ -28,8 +29,10 @@ int main()
     LOP::profiler_enable();
 
     LOP::emit_begin_meta_event("start request", pack_context_to_meta("client"));
+    LOP::emit_flow_start_event("start thread flow", pack_context_to_meta("client", 123));
 
     std::thread t1([]() {
+        LOP::emit_flow_finish_event("start thread flow", pack_context_to_meta("server", 123));
         LOP::emit_begin_meta_event("processing request", pack_context_to_meta("server"));
         LOP::emit_begin_meta_event("checking db", pack_context_to_meta("io"));
         volatile uint32_t a = 0;
@@ -44,11 +47,13 @@ int main()
         }
         LOP::emit_end_meta_event("sending response", pack_context_to_meta("network"));
         LOP::emit_end_meta_event("processing request", pack_context_to_meta("server"));
+        LOP::emit_flow_start_event("join thread flow", pack_context_to_meta("server", 456));
     });
 
     LOP::emit_end_meta_event("start request", pack_context_to_meta("client"));
     LOP::emit_begin_meta_event("wait for server", pack_context_to_meta("client"));
     t1.join();
+    LOP::emit_flow_finish_event("join thread flow", pack_context_to_meta("client", 456));
     LOP::emit_end_meta_event("wait for server", pack_context_to_meta("client"));
     LOP::emit_begin_meta_event("process response", pack_context_to_meta("client"));
     LOP::emit_begin_meta_event("dumping", pack_context_to_meta("io"));
